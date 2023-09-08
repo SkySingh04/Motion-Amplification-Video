05dd7ed9bb0045c36083393b02386752a15d5bd6
Akash <akashsingh2210670@gmail.com>
2023-09-08 17:07:11 +0530

    finished userinput and fixed tailwind

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,31 @@
-// Navbar.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; 
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <nav className="bg-blue-500 py-4">
-      <ul className="flex space-x-4">
-        <li><Link to="/" className="text-white">Home</Link></li>
-        <li><Link to="/dashboard" className="text-white">Dashboard</Link></li>
-        <li><Link to="/output" className="text-white">Output</Link></li>
-      </ul>
+    <nav className="bg-dark p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-black font-bold text-2xl">Your Logo</Link>
+        <div className="lg:hidden">
+          {/* Mobile Menu Button */}
+          <button
+            className="text-black hover:text-gray-300 focus:outline-none focus:text-gray-300"
+            onClick={() => alert("Toggle mobile menu here")}
+          >
+            ☰
+          </button>
+        </div>
+        <div className="hidden lg:block space-x-4">
+          <Link to="/" className="text-black">Home</Link>
+          <Link to="/dashboard" className="text-black">Dashboard</Link>
+          <Link to="/about" className="text-black">About</Link>
+          <Link to="/login" className="text-black">Login</Link>
+          <Link to="/signup" className="text-black">Signup</Link>
+          <button className="bg-red-500 text-black px-4 py-2 rounded">Sign Out</button>
+        </div>
+      </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        customGray: {
           light: '#E4E3E3',
           DEFAULT: '#84A9AC',
           dark: '#3B6978',
@@ -14,5 +13,4 @@ module.exports = {
   plugins: [],
 }
 
-  }}
-  
+  }
