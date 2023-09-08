0ffda0cd9289112dbd654b064b105d1ad8664fbf
Navtika Lipi <navtikalipi2002@gmail.com>
2023-09-08 22:38:25 +0530

    Navbar changes to design branch

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
     extend: {
       colors: {
-        customGray: {
           light: '#E4E3E3',
           DEFAULT: '#84A9AC',
           dark: '#3B6978',
@@ -12,5 +11,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
-
+} }
