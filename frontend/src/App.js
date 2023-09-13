cde7f537d6c351cad84c5be97fbe1a642333b044
Akash <akashsingh2210670@gmail.com>
2023-09-13 21:48:24 +0530

    I dont know how but somehow it is fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Upload from './pages/Upload';
 import Output from './pages/Output';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-
+import InputPage from './pages/InputPage';
 
 
 function App() {
@@ -16,6 +16,9 @@ function App() {
           <Route exact path="/" element={ <HomePage />} />
           <Route path="/upload" element={ <Upload/>} />
           <Route path="/output" element={<Output/>} />
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/input" element={<InputPage/>} />
           <Route path="/signup" element={<Signup/>} />
           <Route path="/login" element={<Login/>} />
         </Routes>
