04959cdebe0e911bd7004ba5727ae97071cca75d
Akash <akashsingh2210670@gmail.com>
2023-09-13 21:26:26 +0530

    FInished full cloud integration on upload page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes  } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import Upload from './pages/Upload';
 import Output from './pages/Output';
