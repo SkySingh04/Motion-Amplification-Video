40586d1b10cbac01149bc42350ff0bf20d5c4b33
Akash <akashsingh2210670@gmail.com>
2023-09-14 14:02:57 +0530

    plis style this TT

--- frontend/src/pages/InputPage.jsx
+++ frontend/src/pages/InputPage.jsx
@@ -1,17 +1,46 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import ImportData from '../components/ImportData';
 import VideoAmp from '../components/VideoAmp';
 import FreqSpect from '../components/FreqSpect';
-
+import UserInput from '../components/UserInput';
 const InputPage = () => {
   const [dialogVisible, setDialogVisible] = useState(true);
   const navigate = useNavigate();
+  const location = useLocation();
+  console.log(location.state?.selectedVideo);
 
   const handleCloseDialog = () => {
     setDialogVisible(false);
     navigate('/');
   };
+
+  const selectedVideo = location.state?.selectedVideo;
+
+  useEffect(() => {
+    if (selectedVideo) {
+      // Define the endpoint URL
+      const endpoint = '/input';
+
+      // Create a FormData object and append the selected video to it
+      const formData = new FormData();
+      formData.append('selectedVideo', selectedVideo);
+
+      // Send a POST request to the server
+      fetch(endpoint, {
+        method: 'POST',
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('Server Response:', data);
+          // Handle the server response as needed
+        })
+        .catch((error) => {
+          console.error('Error sending POST request:', error);
+        });
+    }
+  }, [selectedVideo]);
 
   return (
     <div className={`bg-default flex justify-center items-center h-screen ${dialogVisible ? '' : 'hidden'}`}>
@@ -29,6 +58,7 @@ const InputPage = () => {
                 x
             </button>
           </div>
+          <UserInput />
           {/* Rest of the content */}
           <VideoAmp />
           <FreqSpect />
