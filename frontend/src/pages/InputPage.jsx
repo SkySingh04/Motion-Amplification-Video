94b882f224400b3f85be26a8ad3fdd7de6c93d95
Devansh Pehlajani <devanshrockz11@gmail.com>
2023-09-16 02:21:14 +0530

    finally

--- frontend/src/pages/InputPage.jsx
+++ frontend/src/pages/InputPage.jsx
@@ -137,6 +137,7 @@ import UserInput from '../components/UserInput';
 
 const InputPage = () => {
   const [dialogVisible, setDialogVisible] = useState(true);
+  const [loading,setLoading] = useState(false)
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -186,7 +187,7 @@ const InputPage = () => {
 
     // Log the JSON object
     console.log( inputData);
-    
+    setLoading(true)
 
     fetch('http://localhost:8080/upload/', {
       method: 'POST',
@@ -203,6 +204,7 @@ const InputPage = () => {
             data: data
           }
         })
+        setLoading(false)
       })
       .catch((error) => {
         console.error('Error:', error);
@@ -223,6 +225,7 @@ const InputPage = () => {
 
   return (
     <div>
+    {!loading?(<div>
       <Navbar/>
     <div className={`bg-default flex flex-col h-screen justify-center  ${dialogVisible ? '' : 'hidden'}`}>
       <div className="mb-4 p-4">
@@ -246,10 +249,14 @@ const InputPage = () => {
     onClick={handleJSONCreation}
     className="bg-darker text-white px-4 py-2 rounded-md hover:bg-dark"
   >
-    Create JSON
+    MAV
   </button>
 </div>
 </div>
+    </div>):(<div className='flex items-center justify-center h-screen'><svg className="animate-spin -ml-1 mr-3 h-1/3 w-1/3 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg></div>)}
     </div>
   );
 };
