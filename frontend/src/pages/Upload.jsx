fdd76539c218a8d6e41acf9ab074c9b50d5d9550
Devansh633 <106557331+Devansh633@users.noreply.github.com>
2023-09-15 14:29:23 +0530

    Update Upload.jsx

--- frontend/src/pages/Upload.jsx
+++ frontend/src/pages/Upload.jsx
@@ -6,25 +6,6 @@ import DisplayVideo from '../components/DisplayVideo';
 import s3 from '../aws-config';
 import Navbar from '../components/Navbar';
 
-function uploadToS3(selectedVideo) {
-  const params = {
-    Bucket: 'skillissuevid',
-    Key: selectedVideo.name,
-    Body: selectedVideo,
-    ContentType: 'video/mp4',
-  };
-
-  s3.putObject(params, (err, data) => {
-    if (err) {
-      console.error('Error uploading to S3:', err);
-    } else {import React, { useCallback, useState } from 'react';
-import { useNavigate , Link } from 'react-router-dom';
-import Dropzone from '../components/Dropzone';
-import VideoPreview from '../components/VideoPreview';
-import DisplayVideo from '../components/DisplayVideo';
-import s3 from '../aws-config';
-import Navbar from '../components/Navbar';
-
 function Upload() {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
@@ -73,7 +54,7 @@ function Upload() {
         const email = localStorage.getItem("email")
         const params = {
           Bucket: 'skillissuevid',
-          Key: key, // Set the Key based on count
+          Key: key,
           Body: selectedVideo,
           ContentType: 'video/mp4',
         };
@@ -113,77 +94,6 @@ function Upload() {
             selectedVideo={selectedVideo}
             setSelectedVideo={setSelectedVideo}
             key={videoPreviewKey}
-          />
-          {selectedVideo && (
-            <Link 
-            to="/input"
-            state={{
-              selectedVideo: selectedVideo
-            }}
-            >
-            <button
-              className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 mt-4 block"
-              onClick={handleUpload}
-            >
-              Upload and Go to Input
-            </button>
-            </Link>
-            
-          )}
-        </div>
-        <div className="w-3/4 p-4 h-screen">
-          <DisplayVideo selectedVideo={selectedVideo} />
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default Upload;
-
-      console.log('Successfully uploaded to S3:', data);
-    }
-  });
-}
-
-function Upload() {
-  const navigate = useNavigate();
-  
-
-  const [selectedVideo, setSelectedVideo] = useState(null);
-
-  const onDrop = useCallback((acceptedFiles) => {
-    const acceptedVideos = acceptedFiles.filter((file) =>
-      file.name.match(/\.(mp4|avi|mov|mkv|flv|wmv|webm)$/i)
-    );
-
-    if (acceptedVideos.length > 0) {
-      setSelectedVideo(URL.createObjectURL(acceptedVideos[0]));
-      const videoFile = acceptedVideos[0];
-      uploadToS3(videoFile);
-    }
-  }, []);
-
-  const handleUpload = (id) => {
-    if (selectedVideo) {
-      // Create a FormData object to send the selected video
-      navigate("/input" , {
-        state:{
-          selectedVideo: selectedVideo
-        }
-      })
-    }
-  };
-
-  return (
-    <div>
-      <Navbar />
-      <div className="flex">
-        <div className="w-1/4 p-4 h-screen">
-          <Dropzone onDrop={onDrop} />
-          <VideoPreview
-            selectedVideo={selectedVideo}
-            setSelectedVideo={setSelectedVideo}
           />
           {selectedVideo && (
             <Link 
